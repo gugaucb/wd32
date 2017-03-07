68493b1dd78c36a53c8021bc5f44ebc6ab9df204
Gustavo Costa <gugaucb@gmail.com>
2017-03-07 15:55:38 -0300

    parcial aula2

---
+++ js/principal.js
@@ -0,0 +1,19 @@
+var botao = document.querySelector('#mudaLayout');
+var mural = document.querySelector('.mural');
+
+botao.addEventListener('click', mudaLayout);
+botao.addEventListener('click', mudaLabelBotao);
+
+
+function mudaLabelBotao(){
+    if(mural.classList.contains('mural--linhas')){
+        botao.textContent = 'Blocos';    
+    }else{
+        botao.textContent = 'Linhas';
+    }
+}
+
+
+function mudaLayout(){
+    mural.classList.toggle('mural--linhas');
+};
