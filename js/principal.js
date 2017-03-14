95ef86bc14c6d5850ef35f4666511b1ee8990de3
Gustavo Costa <gugaucb@gmail.com>
2017-03-14 17:46:25 -0300

    refactory e colocar trigger personalizada.

--- js/principal.js
+++ js/principal.js
@@ -1,197 +1,21 @@
-var botao = document.querySelector('#mudaLayout');
-var mural = document.querySelector('.mural');
-var botoesRemove = document.querySelectorAll('.opcoesDoCartao-remove');
-var formulario = document.querySelector('.novoCartao');
 
 
-botao.addEventListener('click', mudaLayout);
-botao.addEventListener('click', mudaLabelBotao);
 
 
-function mudaLabelBotao(){
-    if(mural.classList.contains('mural--linhas')){
-        botao.textContent = 'Blocos';    
-    }else{
-        botao.textContent = 'Linhas';
-    }
-}
 
 
-function mudaLayout(){
-    mural.classList.toggle('mural--linhas');
-};
 
 
-$('.opcoesDoCartao-remove').click(removeCartao);
-
-                                 
-function removeCartao(){
-        //this.parentNode.parentNode.remove();
-        var botao = $(this);
-      //  var idCartao = 'cartao_' + botao.data('cartao');
-        
-        //var cartao = $('#'+idCartao).addClass('cartao--sumindo');
-        var cartao = botao.closest('.cartao').addClass('cartao--sumindo');
-        setTimeout(function(){
-            cartao.remove();
-        },400);
-    }
-
-$('.novoCartao').submit(salvaNovoCarta);
- 
- var numeroDeCartoes = $('.cartao').length;
-
-function salvaNovoCarta(evento){ 
-    evento.preventDefault();
-    
-    
-    var campoConteudo = $('.novoCartao-conteudo');
-    var digitado = campoConteudo.val().trim().replace(/\n/g,'<br>');
-    criaCartao(digitado);
-    campoConteudo.val('').focus();
-}
-
-function criaCartao(digitado, cor){
-    cor = cor|| 'yellow';   
-    
-    if(digitado){
-               var tipoCartao = decideTipoCartao(digitado)
-
-                numeroDeCartoes++;
-                var novoCartao = $('<div>').addClass('cartao').addClass(tipoCartao).attr('id', 'cartao_'+(numeroDeCartoes+1)).css('background-color', cor) ;
-                
-                var opcoesDoCartao = $('<div>').addClass('opcoesDoCartao').appendTo(novoCartao);
-
-                $('<button>').addClass('opcoesDoCartao-opcao opcoesDoCartao-remove').text('Remove').appendTo(opcoesDoCartao).data('cartao',numeroDeCartoes+1).click(removeCartao);
-
-                $('<p>').html(digitado).addClass('cartao-conteudo').appendTo(novoCartao);
-                novoCartao.prependTo('.mural');
-
-            }
-
-}
-
-function decideTipoCartao(digitado){
-    console.time('decideTipoCartao');
-    //quebras de linhas
-    var quebras = digitado.split('<br>').length-1;
-    
-    
-    //total de letras
-    var letras = digitado.replace(/<br>| /g,'').length;
-    
-    //tamanho da maior palavra
-    var maiorPalavra = '';
-    var palavras = digitado.replace(/(<br>| )+/g, ' ').split(' ');
-//    $.each(palavras, function(){
-//        var palavra = this;
-//         if(palavra.length > maiorPalavra.length){
-//            maiorPalavra = palavra;
-//        }
-//    });
-    
-        palavras.forEach(function(palavra){
-        if(palavra.length > maiorPalavra.length){
-            maiorPalavra = palavra;
-        }
-    });
-//    for(var i=0; i < palavras.length; i++){
-//        var palavra = palavras[i];
-//        if(palavra.length > maiorPalavra.length){
-//            maiorPalavra = palavra;
-//        }
-//    }
-    
-    if(quebras < 5 && letras < 55 && maiorPalavra.length < 9){
-        return 'cartao--textoGrande';
-    }else if(quebras < 6 && letras < 75 && maiorPalavra.length < 12){
-        return 'cartao--textoMedio';
-    }else{
-        return 'cartao--textoPequeno';
-    }
-   
-    
-    console.timeEnd('decideTipoCartao');
-}
-
-$('#busca').on('input', function(){
-    var digitado = $(this).val().trim();
-    
-    var regex = new RegExp(digitado, 'i');
-    
-    $('.cartao').hide().filter(function(){
-        var conteudo = $(this).find('.cartao-conteudo').text();
-        return regex.test(conteudo); 
-    }).show();
-});
-
-$('#ajuda').one('click', function (){
-   console.time('ajax');
-    $.get('http://ceep.herokuapp.com/cartoes/instrucoes', 
-        function (dados){
-            console.log(dados);
-            $.each(dados.instrucoes, function (){
-                var cartao = this;
-                console.log(cartao);
-                criaCartao(cartao.conteudo, cartao.cor);
-            })
-        });       
-    
-    console.timeEnd('ajax');
-});
-
-$('#sync').click(function (){
-    console.time('post_ajax');
-    $('#sync').removeClass('botaoSync--deuRuim botaoSync--sincronizado');
-    $('#sync').toggleClass('botaoSync--esperando');
-    
-    var cartoes = [];
-    $('.cartao').each(function(){
-        cartao = {}; 
-            
-        cartao.conteudo = $(this).find('.cartao-conteudo').text();
-        cartao.cor = $(this).css('background-color');
-        
-        cartoes.push(cartao);
-        
-    });
-    console.log(cartoes);
-    var mural = {
-        cartoes: cartoes,
-        usuario: 'email@email.com'
-    }
-    
-    $.ajax({
-        
-        url: 'http://ceep.herokuapp.com/cartoes/salvar',
-        method:'POST',
-        data: mural,
-        success: function (resposta){
-        //sucesso
-            
-             $('#sync').addClass('botaoSync--sincronizado');
-            console.log(resposta);
-            },
-        error: function(){
-            $('#sync').addClass('botaoSync--deuRuim');
-        },
-        complete: function (){
-             $('#sync').toggleClass('botaoSync--esperando');
-            console.timeEnd('post_ajax');
-        }
-    });
-    
-    
-});
 
 
-$.getJSON('http://ceep.herokuapp.com/cartoes/carregar?callback=?',
-      {usuario: 'email@email.com'},
-      function (dados) {
-        var cartoes = dados.cartoes;
-        $.each(cartoes, function(){
-            var cartao = this;
-            criaCartao(cartao.conteudo, cartao.cor);
-        })
-      });
+
+
+
+
+
+
+
+
+
+
 
